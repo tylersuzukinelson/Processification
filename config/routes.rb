8f04d7dc9af750c529434a35677188d1681e38b7
tylersuzukinelson <tyler.nelson@alumni.ubc.ca>
2016-10-05 14:30:36 -0700

    Add ability to create estimation session

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :estimation_sessions
+  resources :estimation_sessions, only: [:index, :create, :show]
   resources :estimation_tickets
 
   # You can have the root of your site routed with "root"
