80ddad6e995f80a41a55fd1618693eaaa1740c54
tylersuzukinelson <tyler.nelson@alumni.ubc.ca>
2016-10-05 13:10:52 -0700

    Add team model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005200547) do
+ActiveRecord::Schema.define(version: 20161005201040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 20161005200547) do
   add_index "retro_participants", ["user_id"], name: "index_retro_participants_on_user_id", using: :btree
 
   create_table "retro_sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
